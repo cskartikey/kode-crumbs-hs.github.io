0b3ed0191ccc4c16f3b6abba41a5b52d7256a720
Kartikey S. Chauhan <skartikey314@gmail.com>
2023-10-16 22:18:04 +0530

    Responsiveness

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -10,7 +10,7 @@ const AboutUs = () => {
       </Head>
       <div className="grid grid-cols-1 bg-white dark:bg-kode-crumbs-black px-4 lg:px-24 xl:px-48 py-10 bg-no-repeat bg-cover mt-[72px] lg:mt-0">
         <div className="inline-flex flex-row items-center">
-          <h1 className="font-Billy text-6xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+          <h1 className="font-Billy text-4xl lg:text-5xl 2xl:text-6xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
             About Us
           </h1>
           <Image
@@ -18,17 +18,17 @@ const AboutUs = () => {
             alt="about-us-arrow"
             width={200}
             height={200}
-            className="pt-8 lg:hidden lg:dark:block hidden"
+            className="pt-8 lg:hidden lg:dark:block hidden w-1/4 lg:w-1/6 xl:w-auto pl-2"
           />
           <Image
             src={"/static/svg/about-us-arrow.svg"}
             alt="about-us-arrow"
             width={200}
             height={200}
-            className="pt-8 lg:dark:hidden lg:block hidden"
+            className="pt-8 lg:dark:hidden lg:block w-1/4 lg:w-1/6 xl:w-auto pl-2"
           />
         </div>
-        <p className="dark:text-kode-crumbs-orange text-kode-crumbs-black font-Billy text-2xl font-light">
+        <p className="dark:text-kode-crumbs-orange text-kode-crumbs-black font-Billy text-base lg:text-xl 2xl:text-2xl font-light">
           Kode Crumbs is a place where high school students with a passion for
           technology come together to learn, create, and share their ideas. We
           provide a platform for students to explore their interests in computer
@@ -44,7 +44,7 @@ const AboutUs = () => {
           </Link>
         </p>
         <div className="grid lg:grid-cols-3 py-10 gap-y-10 gap-x-10">
-          <h1 className="font-Billy text-6xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+          <h1 className="font-Billy text-4xl lg:text-5xl 2xl:text-6xl col-span-2 text-kode-crumbs-black dark:text-kode-crumbs-orange">
             Meet the team!
           </h1>
           <div className="flex flex-col items-center lg:col-start-1 gap-y-2">
@@ -55,15 +55,15 @@ const AboutUs = () => {
               height={400}
               className="rounded-lg"
             />
-            <p className="font-Billy font-normal text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-normal text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Kartikey Singh Chauhan
             </p>
-            <p className="font-Billy font-light text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-light text-lg 2xl:text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Founder + Full Stack Developer
             </p>
-            <div className="flex flex-row gap-x-4 text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
+            <div className="flex flex-row gap-x-4 text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
               <Link href={""}>
-                <Icon icon={"ic:round-email"} className="text-4xl" />
+                <Icon icon={"ic:round-email"} className="text-3xl 2xl:text-4xl" />
               </Link>
               <Link href={""}>
                 <Icon icon={"bi:linkedin"} className="" />
@@ -81,13 +81,13 @@ const AboutUs = () => {
               height={400}
               className="rounded-lg"
             />
-            <p className="font-Billy font-normal text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-normal text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Kunwar Utkarsh
             </p>
-            <p className="font-Billy font-light text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-light text-lg 2xl:text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Co-Founder + Graphic Desinger
             </p>
-            <div className="flex flex-row gap-x-4 text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
+            <div className="flex flex-row gap-x-4 text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
               <Link href={""}>
                 <Icon icon={"ic:round-email"} className="text-4xl" />
               </Link>
@@ -107,13 +107,13 @@ const AboutUs = () => {
               height={400}
               className="rounded-lg"
             />
-            <p className="font-Billy font-normal text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-normal text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Shreya Mauyra
             </p>
-            <p className="font-Billy font-light text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-light text-lg 2xl:text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Co-Founder + Marketing Manager
             </p>
-            <div className="flex flex-row gap-x-4 text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
+            <div className="flex flex-row gap-x-4 text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
               <Link href={""}>
                 <Icon icon={"ic:round-email"} className="text-4xl" />
               </Link>
@@ -133,13 +133,13 @@ const AboutUs = () => {
               height={400}
               className="rounded-lg"
             />
-            <p className="font-Billy font-normal text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-normal text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Malaycia Ashely
             </p>
-            <p className="font-Billy font-light text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-light text-lg 2xl:text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Lead UI Designer
             </p>
-            <div className="flex flex-row gap-x-4 text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
+            <div className="flex flex-row gap-x-4 text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
               <Link href={""}>
                 <Icon icon={"ic:round-email"} className="text-4xl" />
               </Link>
@@ -159,13 +159,13 @@ const AboutUs = () => {
               height={400}
               className="rounded-lg"
             />
-            <p className="font-Billy font-normal text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-normal text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Rakrita Rao
             </p>
-            <p className="font-Billy font-light text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-light text-lg 2xl:text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Social Media Manager
             </p>
-            <div className="flex flex-row gap-x-4 text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
+            <div className="flex flex-row gap-x-4 text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
               <Link href={""}>
                 <Icon icon={"ic:round-email"} className="text-4xl" />
               </Link>
@@ -185,13 +185,13 @@ const AboutUs = () => {
               height={400}
               className="rounded-lg"
             />
-            <p className="font-Billy font-normal text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-normal text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Chitransh Verma
             </p>
-            <p className="font-Billy font-light text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
+            <p className="font-Billy font-light text-lg 2xl:text-xl text-kode-crumbs-black dark:text-kode-crumbs-orange">
               Full Stack Developer
             </p>
-            <div className="flex flex-row gap-x-4 text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
+            <div className="flex flex-row gap-x-4 text-2xl 2xl:text-3xl text-kode-crumbs-black dark:text-kode-crumbs-orange items-center ">
               <Link href={""}>
                 <Icon icon={"ic:round-email"} className="text-4xl" />
               </Link>
